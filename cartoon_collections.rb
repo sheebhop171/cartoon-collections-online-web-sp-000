2f967c25265bd60f2fbf1a67a136033a4be65313
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-04-24 20:38:03 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+#names = ["Shelby", "Taylor", "Eric"]
+
+def roll_call_dwarves(names)
+  names.each.with_index(1) do |names,i|
+    puts "#{i}. #{names}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+# planteer_calls = ["earth", "wind", "fire"]
+
+def summon_captain_planet(planteer_calls)
+  planteer_calls.collect {|calls| calls.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? {|i| i.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(item)
   # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+    cheese_types = ["cheddar", "gouda", "camembert"]
+    item.find do |cheese|
+      cheese_types.include?(cheese)
+    end
 end
