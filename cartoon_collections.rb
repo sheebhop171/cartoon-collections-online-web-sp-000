0d8ac5cbf43a46d68f202b5e9335550df45ce434
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-04-24 20:02:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,9 @@ def long_planeteer_calls(calls)
   calls.any? {|i| i.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(strings)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  strings.detect {|i| i = cheese_types}
+  
 end
