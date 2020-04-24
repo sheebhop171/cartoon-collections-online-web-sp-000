d2d73932282d6fd5221688228f97e689f55b583e
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-04-24 20:31:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,6 @@ end
 def find_the_cheese(array)
   # the array below is here to help
     cheese_types = ["cheddar", "gouda", "camembert"]
-    array.detect { |i| i }
-    cheese_types.include?(i)
-
+    array.find do |item|
+      cheese_types.include?(item)
 end
