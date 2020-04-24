ef6f59c45dfe39315d1d893befaf65bb1ba84d25
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-04-24 20:13:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,5 +20,5 @@ def find_the_cheese(cheese_types)
   # the array below is here to help
     cheese_types = ["cheddar", "gouda", "camembert"]
     cheese_types.detect {|i| i}
-    if cheese_types.detect = false
+    if cheese_types.detect == false
 end
